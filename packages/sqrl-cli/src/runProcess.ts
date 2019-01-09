f056c23a8b7bfdcc0fb096428dc8c12282cc9e8b
Josh Yudaken <jyudaken@twitter.com>
2019-01-09 13:13:27 -0800

    Use plain javascript for cli binary

---
+++ packages/sqrl-cli/src/runProcess.ts
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+/**
+ * Copyright 2018 Twitter, Inc.
+ * Licensed under the Apache License, Version 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+// tslint:disable:no-console
+
+import { docopt } from "docopt";
+import { cliMain, CliDoc, getCliOutput } from "./cli/CliMain";
+import { promiseFinally } from "sqrl/lib/jslib/promiseFinally";
+import { CloseableGroup } from "./jslib/Closeable";
+
+export function runProcess() {
+  const args = docopt(CliDoc, {
+    version: 0.1
+  });
+
+  const output = getCliOutput(args);
+  const closeables = new CloseableGroup();
+  let exitCode = 1;
+
+  promiseFinally(
+    cliMain(args, closeables, { output })
+      .catch(err => {
+        output.compileError(err);
+      })
+      .then(() => {
+        exitCode = 0;
+      }),
+    () => {
+      closeables.close();
+      process.exit(exitCode);
+    }
+  );
+}
